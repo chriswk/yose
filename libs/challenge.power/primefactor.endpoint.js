f01bc20148e777aa44d2a6e2bdf0c45d83e216d7
chrkolst <christopher.kolstad@finn.no>
2014-01-31 14:53:10 +0100

    Added guarD

--- libs/challenge.power/primefactor.endpoint.js
+++ libs/challenge.power/primefactor.endpoint.js
@@ -1,11 +1,18 @@
 var primeFactors = function(request, response) {
+	var returnObject = {};
 	var number = request.query.number;
-	var limit = log2(number);
-	var decomposition = [];
-	for (var i = 0; i<limit; i++) {
-		decomposition.push(2);
+	returnObject.number = number;
+	if (isNaN(number)) {
+		returnObject.error = "not a number";
+	} else {
+		var limit = log2(number);
+		var decomposition = [];
+		for (var i = 0; i<limit; i++) {
+			decomposition.push(2);
+		}
+		returnObject.decomposition = decomposition;
 	}
-	response.send({number: number, decomposition: decomposition});
+	response.send(returnObject);
 }
 
 module.exports = primeFactors;
