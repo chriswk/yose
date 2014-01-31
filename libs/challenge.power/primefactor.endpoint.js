d2331c422e76fe37565d93c2f79de5b3dabb19e4
chrkolst <christopher.kolstad@finn.no>
2014-01-31 15:09:55 +0100

    Add too big number

--- libs/challenge.power/primefactor.endpoint.js
+++ libs/challenge.power/primefactor.endpoint.js
@@ -6,6 +6,8 @@ var primeFactors = function(request, response) {
 	returnObject.number = number;
 	if (isNaN(number)) {
 		returnObject.error = "not a number";
+	} else if (number > 1000000) {
+		returnObject.error = "too big number (>1e6)";
 	} else {
 		var limit = prime(number);
 		returnObject.decomposition = limit.split("*").map(function (el) {return parseInt(el)});
