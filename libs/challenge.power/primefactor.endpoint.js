01b5ef94d54b3e02a55525e66b27a375e9cfd178
chrkolst <christopher.kolstad@finn.no>
2014-01-31 15:20:11 +0100

    Handle more than one input

--- libs/challenge.power/primefactor.endpoint.js
+++ libs/challenge.power/primefactor.endpoint.js
@@ -1,22 +1,22 @@
-var prime = require("./primefactors");
+var primeFactor = require("./primefactors");
 
 var primeFactors = function(request, response) {
-	var returnObject = {};
-	var number = request.query.number;
-	returnObject.number = number;
-	if (isNaN(number)) {
-		returnObject.error = "not a number";
-	} else if (number > 1000000) {
-		returnObject.error = "too big number (>1e6)";
-	} else {
-		var limit = prime(number);
-		returnObject.decomposition = limit.split("*").map(function (el) {return parseInt(el)});
+	var returnObjects = [];
+	for (var n in request.query.number) {
+		var returnObject = {};
+		var number = request.query.number[n];
+		returnObject.number = number;
+		if (isNaN(number)) {
+			returnObject.error = "not a number";
+		} else if (number > 1000000) {
+			returnObject.error = "too big number (>1e6)";
+		} else {
+			var limit = primeFactor(number);
+			returnObject.decomposition = limit.split("*").map(function (el) {return parseInt(el)});
+		}
+		returnObjects.push(returnObject);
 	}
-	response.send(returnObject);
+	response.send(returnObjects);
 }
 
 module.exports = primeFactors;
-
-function log2(no) {
-	return Math.log(no) / Math.LN2;
-}
