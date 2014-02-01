d623ca75697fe7cebd43250b8509d47fdc96daa4
Christopher Kolstad <christopher.kolstad@gmail.com>
2014-02-01 13:33:26 +0100

    Unique ui error message

--- libs/challenge.power/primefactor.ui.endpoint.js
+++ libs/challenge.power/primefactor.ui.endpoint.js
@@ -1,7 +1,12 @@
 var singleObject = require('./primefactor.decompose');
 
 var primeFactorsUi = function(req, res) {
-	var result = singleObject(req.query.number);	
+	var result;
+	if (isNaN(req.query.number)) {
+		req.query.number " is not a number";
+	} else {
+	 	result = singleObject(req.query.number);	
+	}
 	res.render('primeFactors', {decomposition: resultString(result)});
 }
 
