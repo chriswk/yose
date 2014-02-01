83a9ede7aaa9f499841a90010340d091ff289064
Christopher Kolstad <christopher.kolstad@gmail.com>
2014-02-01 13:30:51 +0100

    Handle string

--- libs/challenge.power/primefactor.decompose.js
+++ libs/challenge.power/primefactor.decompose.js
@@ -4,7 +4,7 @@ var singleObject = function(number) {
 	var returnObject = {};
 	returnObject.number = number;
 	if (isNaN(number)) {
-		returnObject.error = "not a number";
+		returnObject.error = number + " is not a number";
 	} else if (number > 1000000) {
 		returnObject.error = "too big number (>1e6)";
 	} else {
