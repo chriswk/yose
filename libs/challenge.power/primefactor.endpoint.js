4008a11907f241248e39dd2ff36c5212f5a39bac
Christopher Kolstad <christopher.kolstad@gmail.com>
2014-02-01 14:21:13 +0100

    remember that javascript needs return

--- libs/challenge.power/primefactor.endpoint.js
+++ libs/challenge.power/primefactor.endpoint.js
@@ -2,8 +2,8 @@ var singleObject = require('./primefactor.decompose');
 
 var primeFactors = function(request, response) {
 	if (Array.isArray(request.query.number)) {
-		var returnObjects = request.query.number.map(new function (el) {
-			singleObject(el, false);
+		var returnObjects = request.query.number.map(new function(el) {
+			return singleObject(el, false);
 		});
 	} else {
 		returnObjects = singleObject(request.query.number, false);
