c7b16a1a7c38b73663839118ac7facc351990fab
Christopher Kolstad <christopher.kolstad@gmail.com>
2014-02-01 13:40:44 +0100

    Don't handle negative numbers

--- libs/challenge.power/primefactor.decompose.js
+++ libs/challenge.power/primefactor.decompose.js
@@ -7,6 +7,8 @@ var singleObject = function(number) {
 		returnObject.error = "not a number";
 	} else if (number > 1000000) {
 		returnObject.error = "too big number (>1e6)";
+	} else if (number < 0) {
+		returnObject.error = number + " is not an integer > 1";
 	} else {
 		var limit = primeFactor(number);
 		returnObject.decomposition = limit.split("*").map(function (el) {return parseInt(el)});
