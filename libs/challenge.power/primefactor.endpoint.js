a21a45515acb85857e8b7490f7fc5fde8c990bca
Christopher Kolstad <christopher.kolstad@gmail.com>
2014-02-01 14:22:55 +0100

    ...

--- libs/challenge.power/primefactor.endpoint.js
+++ libs/challenge.power/primefactor.endpoint.js
@@ -2,7 +2,7 @@ var singleObject = require('./primefactor.decompose');
 
 var primeFactors = function(request, response) {
 	if (Array.isArray(request.query.number)) {
-		var returnObjects = request.query.number.map(new function(el) {
+		var returnObjects = request.query.number.map(function(el) {
 			return singleObject(el, false);
 		});
 	} else {
